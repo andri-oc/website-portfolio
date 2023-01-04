a9d382e67851eb8baec47847a074b5289d3d4621
andri-io <a92231374@gmail.com>
2023-01-04 15:13:08 +0800

    add about section

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-AbortSignal
+function toggleDetail(e) {
+  const target = $(e.target);
+
+  if ($(target).hasClass("active")) {
+
+    $(target).html("More info").removeClass("active");
+
+  } else {
+
+    $(target).html("less info").addClass("active");
+    
+  }
+
+  const item = $(target).parents(".about-exp-item");
+  const detail = $(item).children(".about-exp-item-detail");
+
+  $(detail).slideToggle();
+}
