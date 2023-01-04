5a227a4d5948148ece187db970b86b77c013a265
andri-io <a92231374@gmail.com>
2023-01-04 20:55:53 +0800

    add contact section

--- main.js
+++ main.js
@@ -16,3 +16,27 @@ function toggleDetail(e) {
 
   $(detail).slideToggle();
 }
+
+
+function onFormSubmit (e) {
+    e.preventDefault()
+    const email = $("#inp_email");
+    const subject =$("#inp_subject");
+    const message = $("#inp_message");
+
+    if  (!$(email).val()) {
+        alert ("Email is required");
+    } 
+    else if(!$(subject).val()) {
+        alert  ("Subject is required");
+    } 
+    else if (!$(message).val()) {
+        alert ("Message is required");
+    } 
+    else {
+        alert ("form submitted");
+        $(email).val("");
+        $(subject).val("");
+        $(message).val("");
+    }
+}
